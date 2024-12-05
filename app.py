c7b65739a7ee922fa79dbc57f93898c52ef54b58
Nalinda Gamaarachchi <nalindaavishka@gmail.com>
2024-12-05 23:08:16 +0800

    change host port

--- app.py
+++ app.py
@@ -601,4 +601,5 @@ def generate_extracted_pdf(input_path, output_path):
 
 
 if __name__ == '__main__':
-    app.run(debug=True)
+    port = int(os.environ.get("PORT", 5000))
+    app.run(host='0.0.0.0', port=port)
